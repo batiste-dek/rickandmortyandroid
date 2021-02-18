43354d32d752192a766b3d47a047582a672d3d5f
Batiste Dekimpe <batiste.dekimpe.etu@univ-lille.fr>
2021-02-18 10:24:12 +0100

    :sparkles: Create ApiService

---
+++ app/src/main/java/com/example/rickandmorty/data/api/RickAndMortyApiService.java
@@ -0,0 +1,17 @@
+package com.example.rickandmorty.data.api;
+
+import com.example.rickandmorty.data.api.models.RMCharacter;
+import com.example.rickandmorty.data.api.models.RickAndMortySearchResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RickAndMortyApiService {
+    @GET("character/?name={name}")
+    Single<RickAndMortySearchResponse> getCharactersByName(@Query("name") String name);
+
+    @GET("character/${characterId}")
+    Single<RMCharacter> getCharacter(@Query("characterId") int characterId);
+
+}
