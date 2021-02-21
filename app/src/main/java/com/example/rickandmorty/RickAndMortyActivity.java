eb58f5fb8f7d8ac8c0d559bb60786d33549c36b9
Batiste Dekimpe <batiste.dekimpe.etu@univ-lille.fr>
2021-02-21 22:42:27 +0100

    :sparkles: Add ViewPager

--- app/src/main/java/com/example/rickandmorty/RickAndMortyActivity.java
+++ app/src/main/java/com/example/rickandmorty/RickAndMortyActivity.java
@@ -1,14 +1,37 @@
 package com.example.rickandmorty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.rickandmorty.presentation.search.fragment.GridChractersFragment;
+
 public class RickAndMortyActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.view_pager);
+        final GridChractersFragment fragment = new GridChractersFragment();
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        });
     }
+
 }
